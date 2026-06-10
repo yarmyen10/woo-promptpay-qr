0607e0e4e2b3d9fea037f50c09763aca187d3050
yarmyen10 <phongsathon.yen@gmail.com>
2026-06-10 18:12:53 +0700

    wip 🚧 : update woo-promptpay v0.016

--- inc/class-gateway.php
+++ inc/class-gateway.php
@@ -52,7 +52,7 @@ class PromptPay_Gateway extends WC_Payment_Gateway {
 
     public function get_title(): string {
         $icon = '<img src="' . plugin_dir_url(dirname( __FILE__ )) . 'assets/imgs/prompt-pay-logo.jpg"
-            style="width:25%; height:auto; vertical-align:middle; margin-right:8px;" />';
+            style="width:80px; height:auto; vertical-align:middle; margin-right:6px;" />';
 
         return $icon . $this->title;  // ใช้ $this->title แทน hardcode
     }
